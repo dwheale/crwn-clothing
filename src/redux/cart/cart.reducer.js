b1d1f034b769d02727e688c9536c6fe3bb35b2c0
dwheale <reevr1@gmail.com>
2020-04-04 12:14:27 -0400

    created cart item component and connected the cart dropdown with the cartItem reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,26 +1,26 @@
-import CartActionTypes from './cart.types'
-import { addItemToCart } from './cart.utils'
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
   cartItems: []
-}
+};
 
-const CartReducer = (state = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden
-      }
+      };
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default CartReducer
+export default cartReducer;
